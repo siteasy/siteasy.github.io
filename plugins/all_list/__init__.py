4a37442abda1afb23a4e46260817770ad3e28bf7
clker <clarke.lee@163.com>
2017-09-03 20:47:28 +0800

    add list to index

--- plugins/all_list/__init__.py
+++ plugins/all_list/__init__.py
@@ -1,9 +1,9 @@
-import siteasy 
-class Plugin(siteasy.BasePlugin):
+from models import BasePlugin,MainView
+class Plugin(BasePlugin):
     def apply(self,view):
         context = super(Plugin,self).apply(view)
-        print("inst:",siteasy.MainView.instances)
-        print("all_list instance:",self.all_articles)
+        #print("inst:",MainView.instances)
+        #print("all_list instance:",self.all_articles)
         mainview_list = sorted(self.all_articles, key=lambda k : k.date)
         article_list = [{'url':'/'+view.cateview.text+'/'+view.text+'.html','text':view.text,'short_md_content':view.short_md_content} for view in mainview_list]
         for area in self.areas:
